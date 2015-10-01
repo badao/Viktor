53679d2ddb0ca0d6e830966f7925cee592ebca65
badao <huy_vu626@yahoo.com>
2015-10-01 21:21:40 +0700

    Update Program.cs

--- Viktor/Program.cs
+++ Viktor/Program.cs
@@ -35,7 +35,7 @@ namespace Viktor
             _w = new Spell(SpellSlot.W,700);
             _e = new Spell(SpellSlot.E);
             _r = new Spell(SpellSlot.R,700);
-            _r.SetSkillshot(0.25f, 1,float.MaxValue,false,SkillshotType.SkillshotCircle);
+            _r.SetSkillshot(0.25f, 325,float.MaxValue,false,SkillshotType.SkillshotCircle);
             _w.SetSkillshot(0.25f, 325, float.MaxValue, false, SkillshotType.SkillshotCircle);
             E = new Spells(SpellSlot.E, SkillshotType.SkillshotLine, 520, (float)0.25, 40, false, 780, 500);
             //R = new Spells(SpellSlot.R, SkillshotType.SkillshotCircle, 700, 0.25f, 325 / 2, false);
